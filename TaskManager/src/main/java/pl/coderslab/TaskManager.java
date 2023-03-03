b01800eda79b27a6bcc13e859792b4b099de68ca
Grzegorz_Sarnik <sarnik.g@gmail.com>
2023-03-03 18:11:50 +0100

    first commit

---
+++ TaskManager/src/main/java/pl/coderslab/TaskManager.java
@@ -0,0 +1,11 @@
+package pl.coderslab;
+
+public class TaskManager {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World! siema" +
+                "");
+    }
+
+}
